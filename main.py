3b1ade1e05c80cc24b85421110c43e39e6c97beb
andrii <andrijkorolevic@gmail.com>
2022-10-01 11:17:27 +0300

    Created new lab

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("fhg")
+nums = []
+num1 = int(input("->"))
+num2 = int(input("->"))
+
+while num1 >= num2:
+    nums.append(num1)
+    num1 -= 1
+    for i in nums[::-1]:
+        print(i)
+        break
+
+
+while num1 <= num2:
+    nums.append(num2)
+    num2 -= 1
+    for i in nums[::-1]:
+        print(i)
+        break
