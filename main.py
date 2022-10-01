0bffd6fc4cd23d86e189b2c317b5f2438fd10c03
andrii <andrijkorolevic@gmail.com>
2022-10-01 10:09:26 +0300

    Created new lab

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("fhg")
+print("1 число")
+n1 = int(input("->"))
+print("2 число")
+n2 = int(input("->"))
+
+for i in range(n1, n2+1):
+    if i %2==0:
+        print(i)
+
+for i in range(n2+1, n1):
+    if i %2==0:
+        print(i)
