0b42a87a2cf5b040c874f253513de2270773ed50
andrii <andrijkorolevic@gmail.com>
2022-10-01 10:00:01 +0300

    Created new lab

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("fhg")
+print("Введіть 1 число")
+n1 = int(input("->"))
+print("Введіть 2 число")
+n2 = int(input("->"))
+
+for i in range(n1, n2):
+    print(i, end=",")
+
+for i in range(n2, n1):
+    print(i, end=",")
